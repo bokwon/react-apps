fb02be90d2d4d9e75c11c565d113d543710c8a2d
Boyoung Kwon <boyoung.kwon@gmail.com>
2018-04-07 16:29:18 -0700

    create a skeleton for searchable-table app

---
+++ searchable-table/src/index.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+var element = <p>Ready to build React App!</p>;
+
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
