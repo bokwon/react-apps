6853b79d927980d8e6469e18b15e0320c8f58fc9
Boyoung Kwon <boyoung.kwon@gmail.com>
2018-04-07 18:33:59 -0700

    build a static version of app

--- searchable-table/src/index.js
+++ searchable-table/src/index.js
@@ -2,10 +2,100 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-var element = <p>Ready to build React App!</p>;
+class ProductCategoryRow extends React.Component {
+  render() {
+    const category = this.props.category;
+    return (
+      <tr>
+        <td className="category" colSpan="2">{category}</td>
+      </tr>
+    );
+  }
+}
 
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ? product.name : <span className="out-of-stock">{product.name}</span>
+    const price = product.stocked ? product.price : <span className="out-of-stock">{product.price}</span>
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{price}</td>
+      </tr>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search ..." />
+        <span>
+          <input type="checkbox" />
+          Only show products in stock
+        </span>
+      </form>
+    );
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach((product) => {
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow category={product.category} key={product.category} />
+        );
+      }
+      rows.push(
+        <ProductRow product={product} key={product.name} />
+      );
+      lastCategory = product.category;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+class FilterableProductTable extends React.Component {
+  render() {
+    return (
+      <div className="searchable-table">
+        <SearchBar />
+        <ProductTable products={this.props.products}/>
+      </div>
+    );
+  }
+}
+
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 ReactDOM.render(
-  element,
+  <FilterableProductTable products={PRODUCTS} />,
   document.getElementById('root')
 );
