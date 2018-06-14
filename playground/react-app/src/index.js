f4a3f70540eb0e8f68cba763565eea233ffd3df9
Boyoung Kwon <boyoung.kwon@gmail.com>
2018-06-14 13:23:51 -0700

    react composition

--- playground/react-app/src/index.js
+++ playground/react-app/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Calculator from './App';
+import WelcomeDialog from './Dialog';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(<WelcomeDialog />, document.getElementById('root1'));
+
 registerServiceWorker();
