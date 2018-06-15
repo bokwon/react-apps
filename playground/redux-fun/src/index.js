93062326a1bdbe675a14c941dd7283832612ed8f
Boyoung Kwon <boyoung.kwon@gmail.com>
2018-06-15 14:06:10 -0700

    add counter list and deep-freeze package

--- playground/redux-fun/src/index.js
+++ playground/redux-fun/src/index.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import './redux-tutorial/react-counter.js';
+import './redux-tutorial/counter-list.js';
 import registerServiceWorker from './registerServiceWorker';
